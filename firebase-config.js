abc218d442ec944ddb903571be1c5da378869cc1
franknkiwanibusiness <franknkiwanibusiness@gmail.com>
2026-01-14 00:25:30 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFHskUWiyHhZke3KT9kkOtFI_gPsKfiGo",
-    authDomain: "itzhoyoo-f9f7e.firebaseapp.com",
-    projectId: "itzhoyoo-f9f7e",
-    storageBucket: "itzhoyoo-f9f7e.filestorage.app",
-    messagingSenderId: "1094792075584",
-    appId: "1:1094792075584:web:d49e9c3f899d3cd31082a5"
+    apiKey: "AIzaSyCkOQf5wYsXEGQOYu6WtBNaDlgzLkaVh-E",
+    authDomain: "dlsvalue.firebaseapp.com",
+    projectId: "dlsvalue",
+    databaseURL: "https://dlsvalue-default-rtdb.firebaseio.com",
+    storageBucket: "dlsvalue.firebasestorage.app",
+    messagingSenderId: "648591095829",
+    appId: "1:648591095829:web:791e020d1268386a66761c"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
+export const db = getDatabase(app);
