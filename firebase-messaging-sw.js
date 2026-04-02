c4b1cc31b9270919638c2546f6aa1979c507a4ca
franknkiwanibusiness <franknkiwanibusiness@gmail.com>
2026-04-02 17:27:57 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,21 @@
+// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyDFHskUWiyHhZke3KT9kkOtFI_gPsKfiGo",
   projectId: "itzhoyoo-f9f7e",
-  messagingSenderId: "1094792075584",
-  appId: "1:1094792075584:web:d49e9c3f899d3cd31082a5",
+  messagingSenderId: "1055745142104", // Match your main app's sender ID
+  appId: "1:1055745142104:web:8651c6b16e108871408db0",
 });
 
 const messaging = firebase.messaging();
 
-// Handles notifications when the app is in the background or closed
 messaging.onBackgroundMessage((payload) => {
-  console.log('Background message received: ', payload);
-  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon || '/firebase-logo.png'
+    icon: payload.notification.icon || 'https://i.imgur.com/0HqPbxn_d.png'
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
