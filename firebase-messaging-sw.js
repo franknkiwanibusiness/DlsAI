598b95d6b7b5258b70d29b6b7f6c57ae3c1b2335
franknkiwanibusiness <franknkiwanibusiness@gmail.com>
2026-04-02 17:11:55 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,22 +2,22 @@ importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDFHskUWiyHhZke3KT9kkOtFI_gPsKfiGo",
-    authDomain: "itzhoyoo-f9f7e.firebaseapp.com",
-    databaseURL: "https://itzhoyoo-f9f7e-default-rtdb.firebaseio.com",
-    projectId: "itzhoyoo-f9f7e",
-    storageBucket: "itzhoyoo-f9f7e.firebasestorage.app",
-    messagingSenderId: "1055745142104",
-    appId: "1:1055745142104:web:8651c6b16e108871408db0"
+  apiKey: "AIzaSyDFHskUWiyHhZke3KT9kkOtFI_gPsKfiGo",
+  projectId: "itzhoyoo-f9f7e",
+  messagingSenderId: "1094792075584",
+  appId: "1:1094792075584:web:d49e9c3f899d3cd31082a5",
 });
 
 const messaging = firebase.messaging();
 
+// Handles notifications when the app is in the background or closed
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title || "DLS26 UCL Update";
+  console.log('Background message received: ', payload);
+  
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload.notification.body || "Check the tournament lobby!",
-    icon: 'https://i.imgur.com/gzFKr1u.png'
+    body: payload.notification.body,
+    icon: payload.notification.icon || '/firebase-logo.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
